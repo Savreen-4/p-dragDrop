e9f65e5444135bfafe143feaa1a977efbec92662
Savreen-4 <79464605+Savreen-4@users.noreply.github.com>
2021-12-11 19:50:41 +0530

    Add files via upload
    p-dragdrop

---
+++ pages/index.js
@@ -0,0 +1,30 @@
+// pages/client-side.js
+import Head from "next/head";
+import { useEffect } from 'react';
+import ClientOnly from "../components/clientOnly";
+import { default as FirstComponent } from "../components/FormDefault";
+import Drag from "./Drag";
+
+export default function ClientSide() {
+  return (
+    <div>
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link
+          rel="stylesheet"
+          href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css"
+        />
+      </Head>
+
+      <main >
+        <ClientOnly>
+          <FirstComponent />
+          
+         
+         
+          <Drag/>
+        </ClientOnly>
+      </main>
+    </div>
+  );
+}
